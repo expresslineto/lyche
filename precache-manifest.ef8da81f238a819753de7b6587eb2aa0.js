e4dc1f2dae67bbbf6630fb225d40eaa13820f444
hieunugent <hieunugent@gmail.com>
2020-10-22 21:27:42 -0700

    Updates

---
+++ precache-manifest.ef8da81f238a819753de7b6587eb2aa0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10b290428f27682329f4d6be1b07aabb",
+    "url": "/lyche/index.html"
+  },
+  {
+    "revision": "f68f3d5ab340caa26878",
+    "url": "/lyche/static/css/main.8cbddc3d.chunk.css"
+  },
+  {
+    "revision": "4e68a01bcc05ba32e6d4",
+    "url": "/lyche/static/js/2.de0f3f04.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "/lyche/static/js/2.de0f3f04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f68f3d5ab340caa26878",
+    "url": "/lyche/static/js/main.3f5272fe.chunk.js"
+  },
+  {
+    "revision": "4cda2d7bc2c97e5b81a0",
+    "url": "/lyche/static/js/runtime-main.b39338a4.js"
+  },
+  {
+    "revision": "17eb2f4fd44e4d210da1ccb08bbb0fbd",
+    "url": "/lyche/static/media/page1.17eb2f4f.jpg"
+  },
+  {
+    "revision": "ad5d3b562edff81cc8be8d4e68dcc1ff",
+    "url": "/lyche/static/media/page2.ad5d3b56.jpg"
+  },
+  {
+    "revision": "2a59bb46ea66fff870aaf906a1a57b4c",
+    "url": "/lyche/static/media/page3.2a59bb46.jpg"
+  }
+]);
